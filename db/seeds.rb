60bf4f04b2b18ada32ea916e7ca1a05ed3c53fd1
divyasripadham <divya_sri16@yahoo.com>
2015-11-11 12:35:41 -0500

    Checkpoint 3 SignIn SignOut, added seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+member1 = User.create!(
+  email: 'member1@example.com',
+  password: 'helloworld'
+)
+
+member2 = User.create!(
+  email: 'member2@example.com',
+  password: 'helloworld'
+)
