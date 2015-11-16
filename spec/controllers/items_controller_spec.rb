fde2ce5701ecbf9e9de93aad398e41dd1f355ffb
divyasripadham <divya_sri16@yahoo.com>
2015-11-16 11:40:48 -0500

    Checkpoint 7 Mark as completed, Added rspec

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -4,16 +4,13 @@ RSpec.describe ItemsController, type: :controller do
   include Devise::TestHelpers
 
   let(:my_user) { User.create!(email: 'testmember@example.com', password: 'helloworld') }
+  let(:my_item) { Item.create!(name: 'Todo Item', user: my_user) }
 
   before do
     sign_in my_user
   end
-  # let(:my_item) { Item.create!(name: 'Todo Item', user: my_user) }
 
   describe "create item" do
-    # it "increases the number of items by 1" do
-    #   expect{ post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
-    # end
 
     it "flashes notice on success and redirects to user show page" do
       post :create, user_id: my_user.id, item: {name: 'Todo Item'}
@@ -26,6 +23,22 @@ RSpec.describe ItemsController, type: :controller do
       last_item = Item.last
       user_items = my_user.items
       expect(user_items).to include(last_item)
+    end
+
+  end
+
+  describe "destroy item" do
+
+    it "flashes notice on success and redirects to user show page" do
+      delete :destroy, format: :js, user_id: my_user.id, id: my_item.id
+      expect(controller).to set_flash[:notice].to(/Item was deleted/).now
+      expect(response).to have_http_status(:success)
+    end
+
+    it "list of items does not include deleted item for this user" do
+      deleteItem = my_item.id
+      delete :destroy, format: :js, user_id: my_user.id, id: my_item.id
+      expect(my_user.items.where(id: deleteItem)).to be_empty
     end
 
   end
