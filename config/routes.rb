63cd3628512d4ebcdd18835f4ced4f64c8385021
divyasripadham <divya_sri16@yahoo.com>
2016-10-14 13:45:17 -0400

    Few updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'items/create'
-
-  # get 'users/show'
-
-  # get 'home/index'
 
   devise_for :users
 
   root to: "users#show"
 
   resources :users do
-      resources :items, only: [:create, :destroy]
+    resources :items, only: [:create, :destroy]
   end
 
 end
